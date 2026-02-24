3583fef851e5cc092659427e8eebb367aaf7fa55
sfrisuelos <sfrisuelos@gmail.com>
2026-02-24 12:32:04 +0100

    Descomento tablas y valores anteriores para que no fallen los tests

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,8 @@
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
---drop table Carreras;
---create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+drop table Carreras;
+create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
 
 DROP TABLE IF EXISTS historial;
 DROP TABLE IF EXISTS incidencia;
